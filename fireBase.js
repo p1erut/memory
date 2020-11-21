672f8edd9b3a1b1a0eb81e0a39d77db7b14af90d
Maciej Piłat <m.pilat998@gmail.com>
2020-11-21 23:42:02 +0100

    Create fireBase.js
    Just defalut file with firebase

---
+++ fireBase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCvnxywJy3RxW3oio-6EJhfJMQpJlx9S6U",
+    authDomain: "memory-82458.firebaseapp.com",
+    databaseURL: "https://memory-82458.firebaseio.com",
+    projectId: "memory-82458",
+    storageBucket: "memory-82458.appspot.com",
+    messagingSenderId: "746342263203",
+    appId: "1:746342263203:web:a25cbbd8d056934af21216",
+    measurementId: "G-BC8PYL6CDT"
+  };
+
+  firebase.initializeApp(firebaseConfig);
